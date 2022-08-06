08a5e5862be52482df6bbfcb89fc37c0e2f9eded
Chintan Sudani <csudani7@gmail.com>
2022-08-06 16:34:32 +0530

    fix: issue

--- routes/deleteuser.js
+++ routes/deleteuser.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const admin = require("firebase-admin");
 const serviceAccount = require("../serviceAccountKey.json");
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://bikes-rental-a4d84-default-rtdb.firebaseio.com",
 });
-router.get("/", (req, res, next) => {
-  console.log(req.body);
+
+router.get("/", (req, res) => {
   if (!req.query.uid) {
     return res.status(404).send("Invalid route");
   }
